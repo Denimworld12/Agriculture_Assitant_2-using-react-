4ff7fb5de44418364bdfde58a05cf2dc7b8bdc84
Nikhil Gupta <nikhilrakeshg@gmail.com>
2025-03-27 10:55:37 +0530

    deployment process is going here

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: [
       // News sites
@@ -29,6 +30,7 @@ const nextConfig = {
       'www.washingtonpost.com',
       'www.aljazeera.com',
       's.abcnews.com',
+      'placeholder.com',
     ],
     remotePatterns: [
       {
